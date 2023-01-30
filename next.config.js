aeae5323eab1e25bdf3fbbce9d14464aa7a90a26
tinotendachibi <tinotendachibi33@gmail.com>
2023-01-30 10:18:34 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-module.exports={
-  reactStrictMode: false,
+const nextConfig = {
+  reactStrictMode: true,
   swcMinify: true,
-  webpack: (config) => {
-    config.resolve.fallback = {fs: false};
-
-    return config
-  }
 }
+
+module.exports = nextConfig
