72c9f497adc4cab0f42c82d537810aa8a7c84fdc
tinotendachibi <tinotendachibi33@gmail.com>
2023-01-30 10:33:56 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { useEffect ,useState} from 'react'
 import Image from 'next/image'
-import Link from 'next/Link'
+import Link from 'next/link'
 import * as RiIcon from "react-icons/ri"
 import styles from '../styles/Home.module.css'
 import HomeNav from '../Components/Home/HomeNav'
